a33f66d0fbedbd1ad48e7854cc258261bfd2c4a8
Dipika Ranabhat <qubeena7@gmail.com>
2022-08-04 17:41:34 +0545

    one scan per day completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:qr_app/utils/routes/routes.dart';
 import 'package:qr_app/view/home_screen.dart';
 
--- lib/view/qr_scan_screen.dart
+++ lib/view/qr_scan_screen.dart
@@ -1,10 +1,10 @@
 import 'dart:developer';
-import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:qr_app/utils/utils.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 class QRScan extends StatefulWidget {
   const QRScan({Key? key}) : super(key: key);
@@ -15,134 +15,72 @@ class QRScan extends StatefulWidget {
 
 class _QRScanState extends State<QRScan> {
   final qrKey = GlobalKey(debugLabel: 'QR');
-  Barcode? barcode;
-  QRViewController? controller;
 
-  String? todayDate;
+  String todayDate = DateFormat('yyyy-dd-MM').format(DateTime.now());
+
   String? firstText;
   String secondText = "";
 
-  String date = "";
-
+  String savedDate = "";
+  int? count = 0;
+  bool checkQr = false;
   late SharedPreferences prefs;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     getTodaysDate();
   }
 
   @override
   void dispose() {
-    controller?.dispose();
     super.dispose();
   }
 
   @override
-  void reassemble() async {
-    super.reassemble();
-    if (Platform.isAndroid) {
-      await controller!.pauseCamera();
-    }
-    controller!.resumeCamera();
-  }
-
-  //bool usedQR = false;
-
-  @override
   Widget build(BuildContext context) {
-    reassemble();
-
     return SafeArea(
         child: Scaffold(
-      body: Stack(
-        children: [
-          // (date == todayDate)
-          //     ? const Text("You have already scan")
-          //     : buildQRView(context),
-          // Visibility(
-          //   visible: date.isEmpty,
-          //   child: buildQRView(context),
-          // ),
-          (date == todayDate) ? messageBox() : buildQRView(context),
-          // buildQRView(context),
-          // Positioned(
-          //     bottom: 10,
-          //     child: Padding(
-          //         padding: const EdgeInsets.all(8.0), child: buildResult()))
-        ],
-      ),
+      body: MobileScanner(
+          allowDuplicates: false,
+          onDetect: (barcode, args) {
+            if (barcode.rawValue == null) {
+              debugPrint('Failed to scan Barcode');
+            } else {
+              final String code = barcode.rawValue!;
+              debugPrint('Barcode found! $code');
+              checkIfAlreadyScanned(code);
+            }
+          }),
     ));
   }
 
-  Widget buildQRView(context) => QRView(
-        key: qrKey,
-        onQRViewCreated: onQRViewCreated,
-        overlay: QrScannerOverlayShape(
-          borderColor: Colors.white,
-          borderWidth: 10,
-          borderRadius: 15,
-          cutOutSize: MediaQuery.of(context).size.width * 0.8,
-        ),
-      );
-  void onQRViewCreated(QRViewController controller) {
-    setState(() {
-      this.controller = controller;
-      // todayDate = DateFormat('yyyy-dd-MM').format(DateTime.now());
-    });
-    controller.scannedDataStream.listen(
-      (barcode) => setState(() {
-        this.barcode = barcode;
-      }),
-    );
+  checkIfAlreadyScanned(data) {
+    if (savedDate.toString() == todayDate) {
+      Utils.flushBarErrorMessage("You have already scanned", context);
+      //messageBox();
+      log("You have already scanned");
+    } else {
+      log(data);
+      saveTodaysDate();
+      Utils.flushBarErrorMessage("Thank you", context);
+      //thankyoumessageBox();
+    }
   }
 
-  Widget buildResult() {
-    setState(() {
-      //todayDate = true;
-      todayDate = DateFormat('yyyy-dd-MM').format(DateTime.now());
-
-      //firstText = "${barcode}"
-      //log("firstText----${barcode!.code}");
-    });
-    //saveTodaysDate(todayDate!);
-
-    saveTodaysDate(todayDate.toString());
-    log("dateTime----$todayDate");
-    //log("firstText------$firstText");
-    return Container(
-      padding: const EdgeInsets.all(4),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: Colors.white70,
-      ),
-      child: Text(
-        barcode != null ? "${barcode!.code}" : "",
-        maxLines: 3,
-      ),
-    );
-  }
-
-  void saveTodaysDate(
-    String date,
-  ) async {
+  void saveTodaysDate() async {
     prefs = await SharedPreferences.getInstance();
 
-    prefs.setString("todaysDate", date);
-    // prefs.setString("firstText", text);
-
-    //prefs.setString("todaysDate", date);
+    prefs.setString("todaysDate", todayDate);
+    savedDate = todayDate;
   }
 
   void getTodaysDate() async {
     prefs = await SharedPreferences.getInstance();
 
-    date = prefs.getString("todaysDate") ?? "";
-    //secondText = prefs.getString("firstText") ?? "";
-    log("date-----$date");
-    //log("secondText-----$secondText");
-    setState(() {});
+    savedDate = prefs.getString("todaysDate") ?? "";
+
+    log("date-----$savedDate");
   }
 
   Widget messageBox() => Container(
@@ -153,6 +91,19 @@ class _QRScanState extends State<QRScan> {
         ),
         child: const Center(
           child: Text('You already scanned.Cannot scan anymore',
+              style:
+                  TextStyle(fontSize: 15, color: Color.fromARGB(255, 1, 1, 1))),
+        ),
+      );
+
+  Widget thankyoumessageBox() => Container(
+        padding: const EdgeInsets.all(4),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.white70,
+        ),
+        child: const Center(
+          child: Text('Thank you for scanning',
               style:
                   TextStyle(fontSize: 15, color: Color.fromARGB(255, 1, 1, 1))),
         ),
